bf98681704b035b54ac914dd15ebc038768683e2
jparraga76 <57899140+jparraga76@users.noreply.github.com>
2019-11-18 13:21:04 +0100

    Update Greeter.java
    Soluciono problemas de PMD y vuelvo al inicial de Checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,18 +17,9 @@ public class Greeter {
    * Metodo de greet modificado.
    */
   public String greet(String someone) {
-    Long lalala = new Long("2333");
+    //Long lalala = new Long("2333");
     if(MAGICO > 1){
-      if(MAGICO > 1){
-     if(MAGICO > 1){
-     if(MAGICO > 1){
         return String.format("Hello, %s!", someone);
-    }
-       return String.format("Hello, %s!", someone);
-    }
-        return String.format("Hello, %s!", someone);
-    }  
-      return String.format("Hello, %s!", someone);
     }else{
         return String.format("Hello, %s!", someone);
     }
